15409d7f251086926bcc3c08ef25615b3bb099a0
matteo.conterno <matteo.conterno@nttdata.com>
2024-06-25 16:45:21 +0200

    define atomo able to receive input atomico

--- src/atomo.c
+++ src/atomo.c
@@ -1,4 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 int main(int argc, char *argv[]){
-    
-    return 0;
+    if (argc < 2){
+        perror("Necessary at least two inputs");
+        exit(EXIT_FAILURE);
+    }
+
+    int num_atomico = atoi(argv[1]);
+
+    printf("Received num_atomico: %d \n", num_atomico);
+
+    exit(EXIT_SUCCESS);
 }
