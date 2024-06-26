3157696c7b5f996f01556e2c6f6d2d192075ba06
conternovanessa <vconterno2@gmail.com>
2024-06-26 21:44:23 +0200

    aggiunta la scissione e il paramentro di minimo direttamente dalla lettura delle variabili per avitare di doverlo passare tutte le volte che viene creato un nuovo atomo

--- src/atomo.c
+++ src/atomo.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
+#include "util/utils.h"
+
+void scissione(int numero_atomico){
+    int n1 = (rand() % (numero_atomico - 2)) + 2;
+    int n2 = numero_atomico- n1;
+    int new_energy = n1*n2 - ((n1 > n2) ? n1 : n2);
+    printf("n1: %d\n", n1);
+    printf("n2: %d\n", n2);
+
+}
 
 int main(int argc, char *argv[]){
     if (argc < 2){
@@ -10,7 +21,12 @@ int main(int argc, char *argv[]){
 
     int num_atomico = atoi(argv[1]);
 
-    printf("Received num_atomico: %d \n", num_atomico);
+    srand(time(NULL));
+    const char* filename = "variabili.txt";
+    SimulationParameters params = leggiVariabili(filename);
+    int minimo = params.min_n_atomico;
+    scissione(num_atomico);
 
+    printf("Received num_atomico: %d \n", num_atomico);
     exit(EXIT_SUCCESS);
 }
