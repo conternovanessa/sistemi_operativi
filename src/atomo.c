dc6841b414dcecd270bab2f1d86f3374790756fe
conternovanessa <vconterno2@gmail.com>
2024-06-26 22:01:44 +0200

    problema con atomo:/usr/bin/ld: CMakeFiles/atomo.dir/src/atomo.c.o: in function `main':                    atomo.c:(.text+0x100): undefined reference to `leggiVariabili'                    /usr/bin/ld: atomo.c:(.text+0x10a): undefined reference to `print_line'                    /usr/bin/ld: atomo.c:(.text+0x125): undefined reference to `printSimulationParameters'                    collect2: error: ld returned 1 exit status                    make[2]: *** [CMakeFiles/atomo.dir/build.make:97: atomo] Errore 1                    make[1]: *** [CMakeFiles/Makefile2:113: CMakeFiles/atomo.dir/all] Errore 2                    make: *** [Makefile:91: all] Errore 2

--- src/atomo.c
+++ src/atomo.c
@@ -1,8 +1,16 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 #include <time.h>
+#include <semaphore.h>
 #include "util/utils.h"
+
+#define SHARED_MEM_NAME "/shared_mem"
+#define SEMAPHORE_NAME "/semaphore"
+
+shared_data *shm_data;
+sem_t *sem;
+pid_t a_pid,c_pid;
 
 void scissione(int numero_atomico){
     int n1 = (rand() % (numero_atomico - 2)) + 2;
@@ -24,6 +32,10 @@ int main(int argc, char *argv[]){
     srand(time(NULL));
     const char* filename = "variabili.txt";
     SimulationParameters params = leggiVariabili(filename);
+    print_line();
+    printf("PARAMETERS OBTAINED FROM THE FILE: \n");
+    printSimulationParameters(&params);
+
     int minimo = params.min_n_atomico;
 
 
