e50bc33645aa4e615999299e040925428667cb39
matteo.conterno <matteo.conterno@nttdata.com>
2024-07-01 17:52:35 +0200

    open mapped memory and semaphore

--- src/attivatore.c
+++ src/attivatore.c
@@ -2,9 +2,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+#include "util/utils.h"
+
+#define SHARED_MEM_NAME "/shared_mem"
+#define SEMAPHORE_NAME "/semaphore"
 
 int main(int argc, char *argv[]){
-    printf("Attivatore Started!\n");
+    // Open shared memory
+    int shm_fd = shm_open(SHARED_MEM_NAME, O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("shm_open failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Map shared memory
+    shared_data *shm_data = mmap(0, sizeof(shared_data), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (shm_data == MAP_FAILED) {
+        perror("mmap failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Open semaphore
+    sem_t *sem = sem_open(SEMAPHORE_NAME, 0);
+    if (sem == SEM_FAILED) {
+        perror("sem_open failed");
+        exit(EXIT_FAILURE);
+    }
+
+    
+
 
     exit(EXIT_SUCCESS);
 }
