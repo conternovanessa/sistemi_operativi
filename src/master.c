be1572907f7bf330f18cd3306364a6149e903217
matteo.conterno <matteo.conterno@nttdata.com>
2024-07-02 14:44:36 +0200

    moved functionality for shared data

--- src/master.c
+++ src/master.c
@@ -14,9 +14,6 @@
 #include "headers/io.h"
 #include "headers/process.h"
 
-#define SHARED_MEM_NAME "/shared_mem"
-#define SEMAPHORE_NAME "/semaphore"
-
 // Global variables for shared memory and semaphore
 shared_data *shm_data;
 sem_t *sem;
@@ -68,60 +65,6 @@ void terminate_processes(pid_t a_pid, pid_t c_pid) {
     }
 }
 
-void init_shared_memory_and_semaphore() {
-    // Open shared memory
-    int shm_fd = shm_open(SHARED_MEM_NAME, O_CREAT | O_RDWR, 0666);
-    if (shm_fd == -1) {
-        perror("shm_open failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Set size of shared memory
-    if (ftruncate(shm_fd, sizeof(shared_data)) == -1) {
-        perror("ftruncate failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Map shared memory
-    shm_data = mmap(0, sizeof(shared_data), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    if (shm_data == MAP_FAILED) {
-        perror("mmap failed");
-        exit(EXIT_FAILURE);
-    }
-
-    // Initialize shared memory content
-    memset(shm_data, 0, sizeof(shared_data));
-
-    // Create semaphore
-    sem = sem_open(SEMAPHORE_NAME, O_CREAT, 0666, 1);
-    if (sem == SEM_FAILED) {
-        perror("sem_open failed");
-        exit(EXIT_FAILURE);
-    }
-}
-
-void cleanup_shared_memory_and_semaphore() {
-    // Unmap shared memory
-    if (munmap(shm_data, sizeof(shared_data)) == -1) {
-        perror("munmap failed");
-    }
-
-    // Close shared memory
-    if (shm_unlink(SHARED_MEM_NAME) == -1) {
-        perror("shm_unlink failed");
-    }
-
-    // Close semaphore
-    if (sem_close(sem) == -1) {
-        perror("sem_close failed");
-    }
-
-    // Unlink semaphore
-    if (sem_unlink(SEMAPHORE_NAME) == -1) {
-        perror("sem_unlink failed");
-    }
-}
-
 int main(int argc, char *argv[]) {
     const char* filename = "variabili.txt";
     SimulationParameters params = leggiVariabili(filename);
@@ -130,7 +73,7 @@ int main(int argc, char *argv[]) {
     printSimulationParameters(&params);
     
     // Initialize shared memory and semaphore
-    init_shared_memory_and_semaphore();
+    init_shared_memory_and_semaphore(SEMAPHORE_NAME, &sem, SHARED_MEM_NAME, &shm_data);
     print_shared_data(shm_data);
 
     // Create and start the timer
@@ -145,7 +88,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Wait for a certain amount of time
-    sleep(8);  // Wait for 2 seconds
+    sleep(6);  // Wait for 2 seconds
 
     // Print shared data after sleep
     print_shared_data(shm_data);
@@ -162,7 +105,7 @@ int main(int argc, char *argv[]) {
     print_line();
 
     // Cleanup shared memory and semaphore
-    cleanup_shared_memory_and_semaphore();
+    cleanup_shared_memory_and_semaphore(SEMAPHORE_NAME, &sem, SHARED_MEM_NAME, &shm_data);
 
     exit(EXIT_SUCCESS);
 }
