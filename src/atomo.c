42114929d24f0cd097a8fce5f04a60c7034192e3
conternovanessa <vconterno2@gmail.com>
2024-07-08 15:52:09 +0200

    al posto dello switch è stato esso un if else e si sta cercando di aggiornare tutte le variabili della memoria condivisa

--- src/atomo.c
+++ src/atomo.c
@@ -11,55 +11,55 @@
 #include "headers/io.h"
 #include "headers/process.h"
 
-#define  READ_END 0
+#define READ_END 0
 #define WRITE_END 1
+
 shared_data *shm_data;
 sem_t *sem;
 
-void scissione(int numero_atomico, int wite_fd);
+void scissione(int numero_atomico, int write_fd);
 
 void processa_atomo(int minimo, int *atomico) {
-    printf("in processa atomo");
+    printf("in processa atomo\n");
     if(*atomico <= minimo){
         shm_data->scorie++;
-        printf("minimo");
+        printf("minimo\n");
     }else{
+        printf("cercando di fare una scissione \n");
         int pipe_pf[2];
-        if(pipe(pipe_pf)==-1){
+        if(pipe(pipe_pf) == -1){
             perror("pipe failed");
             exit(EXIT_FAILURE);
         }
         pid_t pid = fork();
-        switch(pid){
-            case -1:
-                perror("fork failed");
+        if(pid == -1) {
+            perror("fork failed");
+            exit(EXIT_FAILURE);
+        } else if(pid == 0) {
+            printf("nuovo atomo\n");
+            close(pipe_pf[WRITE_END]);
+            int n1, n2;
+            read(pipe_pf[READ_END], &n1, sizeof(n1));
+            read(pipe_pf[READ_END], &n2, sizeof(n2));
+            if (n1 != n2) {
+                int massimo = (n1 > n2) ? n1 : n2;
+                shm_data->free_energy *= n1 * n2 - massimo;
+            }
+            char buffer[20];
+            sprintf(buffer, "%d", n1);
+            char *atomo_args[] = {"./atomo", buffer, NULL};
+            if (execve("./atomo", atomo_args, NULL) == -1) {
+                perror("execve failed for atomo");
                 exit(EXIT_FAILURE);
-            case 0:
-                printf("nuovo atomo");
-                close(pipe_pf[WRITE_END]);
-                int n1,n2;
-                read(pipe_pf[READ_END],&n1,sizeof(n1));
-                read(pipe_pf[READ_END],&n2,sizeof(n2));
-                if(n1!= n2){
-
-                    int massimo = (n1>n2)?n1:n2;
-                    shm_data -> free_energy *= n1*n2-massimo;
-                }
-                char buffer[20];
-                sprintf(buffer, "%d", n1);
-                char *atomo_args[] = {"atomo", buffer, NULL};
-                if (execve("./atomo", atomo_args, NULL) == -1) {
-                    perror("execve failed for atomo");
-                    exit(EXIT_FAILURE);
-                }
-                *atomico = n2;
-                break;
-            default:
-                close(pipe_pf[READ_END]);
-                scissione(*atomico, pipe_pf[WRITE_END]);
-                close(pipe_pf[WRITE_END]);
-                wait(NULL);
-                break;
+            }
+            *atomico = n2;
+            shm_data->num_processes++;
+        } else {
+            close(pipe_pf[READ_END]);
+            shm_data->scissioni++;
+            scissione(*atomico, pipe_pf[WRITE_END]);
+            close(pipe_pf[WRITE_END]);
+            wait(NULL);
         }
     }
     // Unmap shared memory and close semaphore (unreachable in this example)
@@ -68,19 +68,18 @@ void processa_atomo(int minimo, int *atomico) {
 }
 
 void scissione(int numero_atomico, int write_fd) {
-    int n1 = (rand()%(numero_atomico -2))+1;
-    int n2 = numero_atomico -n1;
-
+    int n1 = (rand() % (numero_atomico - 2)) + 1;
+    int n2 = numero_atomico - n1;
     write(write_fd, &n1, sizeof(n1));
     write(write_fd, &n2, sizeof(n2));
 }
 
-int main(int argc, char *argv[]){
-    if (argc < 2){
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
         perror("Necessary at least two inputs");
         exit(EXIT_FAILURE);
     }
-    const char* filename = "variabili.txt";
+    const char *filename = "variabili.txt";
     SimulationParameters params = leggiVariabili(filename);
 
     int minimo = params.min_n_atomico;
@@ -111,14 +110,8 @@ int main(int argc, char *argv[]){
 
     if (info.si_signo == SIGUSR1) {
         printf("Received SIGUSR1, performing specific action.\n");
-        processa_atomo(minimo,&num_atomico);
+        processa_atomo(minimo, &num_atomico);
     }
 
     exit(EXIT_SUCCESS);
 }
-
-
-
-
-
-
