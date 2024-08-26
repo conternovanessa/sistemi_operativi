ec8fcbbf43cd6bf33f4a66f8172d733f892693fe
matinator <matteoconterno97@gmail.com>
2024-08-26 22:26:47 +0200

    add policy to create a random number of activations

--- src/attivatore.c
+++ src/attivatore.c
@@ -13,6 +13,7 @@
 
 shared_data *shm_data;
 sem_t *sem;
+SimulationParameters params;
 
 void send_signal() {
     // Seed the random number generator
@@ -20,19 +21,27 @@ void send_signal() {
     
     pid_t receiver_pid = 0;
     int random_index = 0;
-    
-    random_index = rand() % shm_data->num_processes;
-    receiver_pid = shm_data->pid_array[random_index];
-    
+
+    int new_atom_random = (rand() % params.n_atom_init) + 1;
+
     sem_wait(sem);
-    shm_data->attivazioni++;
-    sem_post(sem);
-    
-    // Send SIGUSR1 to the randomly selected receiver process
-    if (kill(receiver_pid, SIGUSR1) == -1) {
-        perror("kill");
-        exit(EXIT_FAILURE);
+    for(int m = 0; m < new_atom_random; m++){
+
+        random_index = rand() % shm_data->num_processes;
+        receiver_pid = shm_data->pid_array[random_index];
+        
+        
+        shm_data->attivazioni++;
+        
+        
+        // Send SIGUSR1 to the randomly selected receiver process
+        if (kill(receiver_pid, SIGUSR1) == -1) {
+            perror("kill");
+            exit(EXIT_FAILURE);
+        }
     }
+    sem_post(sem);
+
 }
 
 // Timer signal handler
@@ -49,7 +58,7 @@ void sigterm_handler(int sig) {
 int main(int argc, char *argv[]){
 
     const char* filename = "variabili.txt";
-    SimulationParameters params = leggiVariabili(filename);
+    params = leggiVariabili(filename);
 
     // Open shared memory
     connect_shared_memory_and_semaphore(SEMAPHORE_NAME, &sem, SHARED_MEM_NAME, &shm_data);
